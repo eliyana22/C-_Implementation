f1dc94e17e7bf81dd1fd48a302811069c215a308
Caballero A <91328335+eliyana22@users.noreply.github.com>
2023-02-13 19:40:57 +0800

    Other Practices

---
+++ 1,2,3_what/1,2,3_what/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_2_3_what
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            String num = Console.ReadLine();
+
+            for (int i = 0; i < num.Length; i++) {
+                if ((num[i] - 48) > 3) {
+                    Console.WriteLine(num[i]);
+                    flag++;
+                }
+            }
+
+            if (flag == 0) {
+                Console.WriteLine("none");
+            }
+        }
+    }
+}
