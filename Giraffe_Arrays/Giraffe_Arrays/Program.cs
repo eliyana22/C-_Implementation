32441b77f72f2c1c3877e15474acdc11dd067473
Caballero A <91328335+eliyana22@users.noreply.github.com>
2023-02-13 19:43:58 +0800

    Giraffe Problem

---
+++ Giraffe_Arrays/Giraffe_Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Giraffe_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARRAY OF INTEGER 
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            Console.WriteLine("Lucky Number in Arrays : " + luckyNumbers[2]);
+
+            // ARRAY OF STRING THAT HAS 10 VALUE
+            String[] friends = new string[10];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.WriteLine("\nName of Friend : " + friends[0]);
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Giraffe_Calculator/Giraffe_Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Giraffe_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          /*  
+            // GET INTEGER
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+          */
+
+            // GET DOUBLE
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("THE SUM OF TWO NUMBER IS " + num1 + num2);
+        }
+    }
+}
